f4d902dbd5707c428444f93f1236e3f0686135ae
Justin Christian <justinchristian2016@gmail.com>
2022-08-23 23:02:01 +0700

    FE9: comments components

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,24 +1,32 @@
 import React from 'react'
 import Image from 'next/image'
 
-export default function Comments() {
+// Schema Data Comments
+// const dat2 = {
+//     name: "Audrey L",
+//     title: "Aktivis Community Development",
+//     testimoni: "Proin pretium luctus ligula eget tempor. Vivamus pharetra elementum dui non posuere. Phasellus tristique dolor massa, sit amet cursus purus tincidunt quis. Duis tempus ante sit amet eros tempor, quis consequat enim imperdiet.",
+//     img: "/Ellipse.png",
+// }
+
+export default function Comments({ data } : any) {
     return (
         <>
-            <div>
-                <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias eligendi nobis
-                    dolorum magni distinctio iusto explicabo voluptatibus sint, esse et, voluptatem
-                    aperiam error id sapiente earum dolorem placeat ad quisquam.
-                </p>
-                <div className="">
+            <div className="w-[397px] overflow-hidden text-sm relative bg-white rounded-xl shadow-xl">
+                <p className="overflow-hidden p-[32.5px]">{data.testimoni}</p>
+                <div className="flex px-[32.5px] pb-[32.5px] items-center">
                     <Image
-                        src={"/9k.png"}
+                        src={data.img}
                         alt="photo"
-                        width={300}
-                        height={300}
+                        width={64}
+                        height={64}
                         objectFit="cover"
                         className="rounded-full"
                     />
+                    <div className="px-[17px]">
+                        <h4 className="font-bold">{data.name}</h4>
+                        <h5 className="font-semibold">{data.title}</h5>
+                    </div>
                 </div>
             </div>
         </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,17 @@ import styles from "../styles/Home.module.css"
 
 import Comments from "../components/Comments"
 
+const dat2 = {
+    name: "Audrey L",
+    title: "Aktivis Community Development",
+    testimoni: "Proin pretium luctus ligula eget tempor. Vivamus pharetra elementum dui non posuere. Phasellus tristique dolor massa, sit amet cursus purus tincidunt quis. Duis tempus ante sit amet eros tempor, quis consequat enim imperdiet.",
+    img: "/Ellipse.png",
+}
+
 const Home: NextPage = () => {
-    return <Comments />
+    return <>
+            <Comments data={dat2} />
+    </>
 }
 
 export default Home
