47051dd0a354469696dd7d0f0d35fc6048ef2a9a
Justin Christian <justinchristian2016@gmail.com>
2022-08-24 00:59:24 +0700

    add interface, update package lock

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -10,19 +10,21 @@ import Image from "next/image"
 // }
 
 interface commentsData {
-    name: string
-    title: string
-    testimoni: string
-    img: string
+    commentdata: {
+        name: string
+        title: string
+        testimoni: string
+        img: string
+    }
 }
 
 export default function Comments(data: commentsData) {
     return (
         <div className="w-[397px] overflow-hidden text-sm relative bg-white rounded-xl shadow-xl">
-            <p className="overflow-hidden p-[32.5px]">{data.testimoni}</p>
+            <p className="overflow-hidden p-[32.5px]">{data.commentdata.testimoni}</p>
             <div className="flex px-[32.5px] pb-[32.5px] items-center">
                 <Image
-                    src={data.img}
+                    src={data.commentdata.img}
                     alt="photo"
                     width={64}
                     height={64}
@@ -30,8 +32,8 @@ export default function Comments(data: commentsData) {
                     className="rounded-full"
                 />
                 <div className="px-[17px]">
-                    <h4 className="font-bold">{data.name}</h4>
-                    <h5 className="font-semibold">{data.title}</h5>
+                    <h4 className="font-bold">{data.commentdata.name}</h4>
+                    <h5 className="font-semibold">{data.commentdata.title}</h5>
                 </div>
             </div>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,14 +8,17 @@ import Comments from "../components/Comments"
 const dat2 = {
     name: "Audrey L",
     title: "Aktivis Community Development",
-    testimoni: "Proin pretium luctus ligula eget tempor. Vivamus pharetra elementum dui non posuere. Phasellus tristique dolor massa, sit amet cursus purus tincidunt quis. Duis tempus ante sit amet eros tempor, quis consequat enim imperdiet.",
+    testimoni:
+        "Proin pretium luctus ligula eget tempor. Vivamus pharetra elementum dui non posuere. Phasellus tristique dolor massa, sit amet cursus purus tincidunt quis. Duis tempus ante sit amet eros tempor, quis consequat enim imperdiet.",
     img: "/Ellipse.png",
 }
 
 const Home: NextPage = () => {
-    return <>
-            <Comments data={dat2} />
-    </>
+    return (
+        <>
+            <Comments commentdata={dat2} />
+        </>
+    )
 }
 
 export default Home
