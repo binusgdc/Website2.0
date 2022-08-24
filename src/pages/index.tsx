576ac46789b684fb6bb09dddb2c03b6e86982e43
Justin Christian <justinchristian2016@gmail.com>
2022-08-24 12:57:43 +0700

    deleted unused import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
 import type { NextPage } from "next"
-import Head from "next/head"
-import Image from "next/image"
-import styles from "../styles/Home.module.css"
 
 import Comments from "../components/Comments"
 
@@ -10,7 +7,7 @@ const dat2 = {
     title: "Aktivis Community Development",
     testimoni:
         "Proin pretium luctus ligula eget tempor. Vivamus pharetra elementum dui non posuere. Phasellus tristique dolor massa, sit amet cursus purus tincidunt quis. Duis tempus ante sit amet eros tempor, quis consequat enim imperdiet.",
-    img: "/Ellipse.png",
+    img: "/images/Ellipse.png",
 }
 
 const Home: NextPage = () => {
