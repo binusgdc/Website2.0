efb064b4c1f2bf10ad86da8aeefa5612474aa367
500-internal-server-error <76838083+500-internal-server-error@users.noreply.github.com>
2022-09-16 22:29:55 +0700

    Implemented user update API

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,7 +1,77 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
+import {
+    checkRequestBody,
+    checkRequestHeader,
+    checkSuppliedId,
+    ContentType,
+    greet,
+    PackedRequestData,
+    printRequest,
+    respond,
+    sendGenericMalformedRequestBodyResponse,
+    sendIllegalMethodResponse,
+    sendMultipleSuppliedIdsErrorResponse,
+    sendUpdateResultResponse,
+} from "../../libs/apiHandling"
 import connect from "../../libs/database"
+
+import User, { userSchemaKeys } from "../../model/user"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     await connect()
+
+    const client = `${req.socket.remoteAddress}:${req.socket.remotePort}`
+    const r: PackedRequestData = { client, req, res }
+    const resp = (code: number) => {
+        respond(r, code)
+    }
+
+    greet(r)
+
+    switch (req.method) {
+        case "POST":
+            {
+                //
+            }
+            break
+
+        case "PUT":
+            {
+                if (!checkRequestHeader(r, ContentType.JSON)) return
+                printRequest(r)
+
+                if (!checkSuppliedId(r)) {
+                    sendMultipleSuppliedIdsErrorResponse(r, "User", "/^U\\d+$/g")
+                    return
+                }
+
+                if (!checkRequestBody(r, userSchemaKeys)) return
+
+                try {
+                    sendUpdateResultResponse(
+                        r,
+                        await User.updateMany(
+                            { _id: req.body._id },
+                            { $set: req.body },
+                            { runValidators: true }
+                        )
+                    )
+                } catch (err: any) {
+                    sendGenericMalformedRequestBodyResponse(r, err.message)
+                    return
+                }
+            }
+            break
+
+        case "DELETE":
+            {
+                //
+            }
+            break
+
+        default:
+            sendIllegalMethodResponse(r)
+            break
+    }
 }
