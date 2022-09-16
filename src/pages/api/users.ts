2a2a5d13f5216a50d349f28e0eb6328041b40f6a
500-internal-server-error <76838083+500-internal-server-error@users.noreply.github.com>
2022-09-16 16:31:29 +0700

    Defined user model for user CRUD API

---
+++ src/pages/api/users.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from "next"
+
+import connect from "../../libs/database"
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	await connect()
+}
