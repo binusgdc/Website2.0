12296c0ef27f6ace9d42ca3dea3d899b13df184f
500-internal-server-error <76838083+500-internal-server-error@users.noreply.github.com>
2022-09-16 20:28:39 +0700

    Ran linter

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -3,5 +3,5 @@ import { NextApiRequest, NextApiResponse } from "next"
 import connect from "../../libs/database"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	await connect()
+    await connect()
 }
