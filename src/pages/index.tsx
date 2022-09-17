cd19c8a2affbcc17d3fc77b2969934475315d48c
Pixelshiftx <vitosuyata@gmail.com>
2022-09-17 11:49:16 +0700

    Landing Page (1st push fix)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,4 @@
-<<<<<<< Updated upstream
-import type { NextPage } from "next"
 
-import Comments from "../components/Comments"
-
-const dat2 = {
-    name: "Audrey L",
-    title: "Aktivis Community Development",
-    testimoni:
-        "Proin pretium luctus ligula eget tempor. Vivamus pharetra elementum dui non posuere. Phasellus tristique dolor massa, sit amet cursus purus tincidunt quis. Duis tempus ante sit amet eros tempor, quis consequat enim imperdiet.",
-    img: "/images/Ellipse.png",
-}
-
-const Home: NextPage = () => {
-    return (
-        <>
-            <Comments commentdata={dat2} />
-        </>
-    )
-=======
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
@@ -55,7 +36,7 @@ const Home: NextPage = () => {
       </div>
     </div>
   )
->>>>>>> Stashed changes
+
 }
 
 export default Home
