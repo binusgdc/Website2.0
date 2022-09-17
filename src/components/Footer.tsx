086a18ce81fbfd7fe82cd425a422dc799aebbbe7
Pixelshiftx <vitosuyata@gmail.com>
2022-09-17 11:45:04 +0700

    Landing Page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,270 @@
+const Footer = () => {
+    return (
+        <>
+        <footer className="   dark:bg-neutral-700 p-20 pt-24 lg:p-16">
+            <div className=" lg:flex  justify-center ">
+                <div className="hidden lg:block">
+                    <div className="grid grid-cols-3 gap-3">
+                        <div>
+                            <a>
+                                <img src="/binus2.png" className="mt-1 mr-4 w-15 h-12 max-w-min" alt=""/>
+                            </a>
+                        </div>
+
+                        <div className="ml-14 ">
+                            <ul className="text-white dark:text-white text-xs min-w-max">
+                                <li className="mb-1">
+                                    <a>Jl. K. H. Syahdan No. 9, Kemanggisan,</a>
+                                </li>
+                                <li className="mb-1">
+                                    <a>Palmerah Jakarta 11480</a>
+                                </li>
+                                <li className="mb-1">
+                                    <a>Indonesia</a>
+                                </li>
+                                <i className="fa-brands fa-itch-io"></i>
+                            </ul>
+                        </div>
+                        <div className="ml-14">
+                            <ul className="text-white dark:text-white text-xs font-medium ">
+                                <li className="mb-1">
+                                    <a href="#" className="hover:underline">
+                                        Privacy
+                                    </a>
+                                </li>
+
+                                <li className="mb-1">
+                                    <a href="#" className="hover:underline">
+                                        Brand Guidelines
+                                    </a>
+                                </li>
+                                <li className="mb-1">
+                                    <a href="#" className="hover:underline">
+                                        Sitemap
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="mt-12">
+                        
+                        <span className="static text-white lg:text-center dark:text-white text-xs">
+                            Copyright © 2021. Binus Game Development Club . All Rights Reserved.
+                        </span>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="hidden justify-end lg:flex">
+                        <a>
+                            <img src="/binus1.png" className="h-16 max-w-min" alt="" />
+                        </a>
+                    </div>
+
+                    <div className="lg:hidden">
+                        <a>
+                            <img src="/playobservedevelop.png" className="h-36 max-w-min" alt="" />
+                        </a>
+                    </div>
+
+                    <div className="flex space-x-7 lg:mt-12 lg:space-x-4 mt-6">
+                        <a
+                            href="#"
+                            className="text-white hover:text-gray-900 dark:hover:text-white"
+                        >
+                            <svg
+                                className="w-7 h-7 lg:w-5 lg:h-5 "
+                                fill="currentColor"
+                                viewBox="0 0 24 24"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                            <span className="sr-only">Instagram page</span>
+                        </a>
+
+                        <a
+                            href="#"
+                            className="text-white hover:text-gray-900 dark:hover:text-white"
+                        >
+                            <svg
+                                className="w-7 h-7 lg:w-5 lg:h-5"
+                                fill="currentColor"
+                                viewBox="0 0 512 512"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M71.92 34.77C50.2 47.67 7.4 96.84 7 109.73v21.34c0 27.06 25.29 50.84 48.25 50.84 27.57 0 50.54-22.85 50.54-50 0 27.12 22.18 50 49.76 50s49-22.85 49-50c0 27.12 23.59 50 51.16 50h.5c27.57 0 51.16-22.85 51.16-50 0 27.12 21.47 50 49 50s49.76-22.85 49.76-50c0 27.12 23 50 50.54 50 23 0 48.25-23.78 48.25-50.84v-21.34c-.4-12.9-43.2-62.07-64.92-75C372.56 32.4 325.76 32 256 32S91.14 33.1 71.92 34.77zm132.32 134.39c-22 38.4-77.9 38.71-99.85.25-13.17 23.14-43.17 32.07-56 27.66-3.87 40.15-13.67 237.13 17.73 269.15 80 18.67 302.08 18.12 379.76 0 31.65-32.27 21.32-232 17.75-269.15-12.92 4.44-42.88-4.6-56-27.66-22 38.52-77.85 38.1-99.85-.24-7.1 12.49-23.05 28.94-51.76 28.94a57.54 57.54 0 0 1-51.75-28.94zm-41.58 53.77c16.47 0 31.09 0 49.22 19.78a436.91 436.91 0 0 1 88.18 0C318.22 223 332.85 223 349.31 223c52.33 0 65.22 77.53 83.87 144.45 17.26 62.15-5.52 63.67-33.95 63.73-42.15-1.57-65.49-32.18-65.49-62.79-39.25 6.43-101.93 8.79-155.55 0 0 30.61-23.34 61.22-65.49 62.79-28.42-.06-51.2-1.58-33.94-63.73 18.67-67 31.56-144.45 83.88-144.45zM256 270.79s-44.38 40.77-52.35 55.21l29-1.17v25.32c0 1.55 21.34.16 23.33.16 11.65.54 23.31 1 23.31-.16v-25.28l29 1.17c-8-14.48-52.35-55.24-52.35-55.24z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                            <span className="sr-only">Itch</span>
+                        </a>
+
+                        <a
+                            href="#"
+                            className="text-white hover:text-gray-900 dark:hover:text-white"
+                        >
+                            <svg
+                                className="w-7 h-7 lg:w-5 lg:h-5"
+                                fill="currentColor"
+                                viewBox="0 0 24 24"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                            <span className="sr-only">LinkedIn Page</span>
+                        </a>
+
+                        <a
+                            href="#"
+                            className="text-white hover:text-gray-900 dark:hover:text-white"
+                        >
+                            <svg
+                                className="w-7 h-7 lg:w-5 lg:h-5 "
+                                fill="currentColor"
+                                viewBox="0 0 16 16"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                            <span className="sr-only">YouTube Page</span>
+                        </a>
+
+                        <a
+                            href="#"
+                            className="text-white hover:text-gray-900 dark:hover:text-white"
+                        >
+                            <svg
+                                className="w-7 h-7 lg:w-5 lg:h-5 "
+                                fill="currentColor"
+                                viewBox="0 0 24 24"
+                                aria-hidden="true"
+                            >
+                                <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                            </svg>
+                            <span className="sr-only">Twitter page</span>
+                        </a>
+
+                        <a
+                            href="#"
+                            className="text-white hover:text-gray-900 dark:hover:text-white"
+                        >
+                            <svg
+                                className="w-7 h-7 lg:w-5 lg:h-5 "
+                                fill="currentColor"
+                                viewBox="0 0 24 24"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                            <span className="sr-only">Facebook page</span>
+                        </a>
+                    </div>
+
+                    <div className="lg:hidden mt-10 ">
+                        <ul className="text-white dark:text-white text-lg min-w-max">
+                            <li className="">
+                                <a>Jl. K. H. Syahdan No. 9, Kemanggisan,</a>
+                            </li>
+                            <li className="">
+                                <a>Palmerah Jakarta 11480</a>
+                            </li>
+                            <li className="">
+                                <a>Indonesia</a>
+                            </li>
+                            <i className="fa-brands fa-itch-io"></i>
+                        </ul>
+
+                        <div className="mt-12 text-amber-300 text-lg min-w-max mb-2">
+                            <p>BGDC</p>
+                        </div>
+
+                        <div className="grid grid-cols-2 gap-32 ">
+                            <ul className="text-white dark:text-white text-lg min-w-max">
+                                <li className="mb-2">
+                                    <a href="#" className="hover:underline">
+                                        About Us
+                                    </a>
+                                </li>
+
+                                <li className="mb-2">
+                                    <a href="#" className="hover:underline">
+                                        Events
+                                    </a>
+                                </li>
+                                <li className="mb-2">
+                                    <a href="#" className="hover:underline">
+                                        Products
+                                    </a>
+                                </li>
+                                <li className="mb-2">
+                                    <a href="#" className="hover:underline">
+                                        Blogs
+                                    </a>
+                                </li>
+                            </ul>
+                            <ul className="text-white dark:text-white text-lg min-w-max">
+                                <li className="mb-2">
+                                    <a href="#" className="hover:underline">
+                                        Registration
+                                    </a>
+                                </li>
+                                <li className="mb-2">
+                                    <a href="#" className="hover:underline">
+                                        Partnership
+                                    </a>
+                                </li>
+                                <li className="mb-2">
+                                    <a href="#" className="hover:underline">
+                                        Contact Us
+                                    </a>
+                                </li>
+
+                                <li className="mb-2">
+                                    <a href="#" className="hover:underline">
+                                        Binus Page
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="mt-12 border-rounded border border-amber-400 border-t-2 border-l-0 border-r-0 border-b-0 min-w-max "></div>
+                    </div>
+
+                    <div className="flex justify-between content-center h-2 mt-12 mb-12 lg:mt-0 lg:mb-0">
+                        <div className="lg:hidden">
+                            <a>
+                                <img src="/binus1.png" className="h-16 max-w-min" alt="" />
+                            </a>
+                        </div>
+
+                        <div className="lg:hidden">
+                            <a>
+                                <img src="/binus2.png" className="h-12 max-w-min " alt="" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+</>);
+};
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,68 @@
+import styles from '../styles/Header.module.scss'
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineCloseSquare } from "react-icons/ai";
+import { useState } from 'react';
+import React from "react";
+
+const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const menuToggler = () => setMenuOpen((p) => !p);
+
+    return (
+        <div className={styles.header}>
+            <div className={styles.header__content}>
+                <div className={styles.logo}>
+                    <img className={styles.image} src ="/logo.png" >
+                </img>
+                </div>
+                <div>
+                    <nav className={`${styles.nav} ${menuOpen ? styles[`nav-open`] : {}}`}>
+                        <a className={styles.nav__item} href={"/"}>
+                            Registration
+                        </a>
+                        <a className={styles.nav__item} href={"/"}>
+                            Calendars
+                        </a>
+                        <a className={styles.nav__item} href={"/"}>
+                            Products
+                        </a>
+                        <a className={styles.nav__item} href={"/"}>
+                            Blog
+                        </a>
+                        <a className={styles.nav__item} href={"/"}>
+                            About Us
+                        </a>
+                        <a className={styles.nav__item} href={"/"}>
+                           Contact Us
+                        </a>
+                        <div className={styles.nav__button__container}>
+                            <Button />
+                        </div>
+                    </nav>
+                </div>
+                <div>
+                    <div className={styles.header__button__container}>
+                        <Button />
+                    </div>
+                    <button className={styles.header__toggler} onClick={menuToggler}>
+                        {!menuOpen ? <BiMenuAltRight /> : <AiOutlineCloseSquare />}
+                    </button>
+                </div>
+            </div>
+        </div>
+        
+    );
+};
+
+const Button = () => {
+    return <button className={styles.button}>Login</button>;
+};
+
+// const Header = () => {
+//     return (
+//         <Header>
+
+//         </Header>
+//         );
+//     }
+    export default Header;
