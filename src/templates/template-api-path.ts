92a9772cb1276329e377e3d68ab0d83106013e36
500-internal-server-error <76838083+500-internal-server-error@users.noreply.github.com>
2022-09-24 23:45:17 +0700

    Ran linter

--- src/templates/template-api-path.ts
+++ src/templates/template-api-path.ts
@@ -7,119 +7,121 @@ import ModelledObject from "./template-model"
 const expectedContentType = "application/json"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	const contentType = req.headers["content-type"]
-	if (contentType !== expectedContentType) {
-		res.status(415).json({
-			status: "415 Unsupported Media Type",
-			message: `Expecting Content-Type ${expectedContentType}, received ${contentType}`
-		})
+    const contentType = req.headers["content-type"]
+    if (contentType !== expectedContentType) {
+        res.status(415).json({
+            status: "415 Unsupported Media Type",
+            message: `Expecting Content-Type ${expectedContentType}, received ${contentType}`,
+        })
 
-		return
-	}
+        return
+    }
 
-	const method = req.method
-	switch(method) {
-		case "POST":
-			await connect()
+    const method = req.method
+    switch (method) {
+        case "POST":
+            await connect()
 
-			try {
-				await ModelledObject.create(req.body)
+            try {
+                await ModelledObject.create(req.body)
 
-				res.status(201).json({
-					status: "201 Created",
-					message: "Successfully created object",
-					receivedData: req.body
-				})
+                res.status(201).json({
+                    status: "201 Created",
+                    message: "Successfully created object",
+                    receivedData: req.body,
+                })
 
-				return
-			} catch (err) {
-				res.status(400).json({
-					status: "400 Bad Request",
-					message: (err as MongooseError).message,
-					receivedData: req.body
-				})
+                return
+            } catch (err) {
+                res.status(400).json({
+                    status: "400 Bad Request",
+                    message: (err as MongooseError).message,
+                    receivedData: req.body,
+                })
 
-				return
-			}
+                return
+            }
 
-		case "PUT":
-			await connect()
+        case "PUT":
+            await connect()
 
-			try {
-				const updateRes = await ModelledObject.updateOne({ _id: req.body._id }, req.body, { runValidators: true })
+            try {
+                const updateRes = await ModelledObject.updateOne({ _id: req.body._id }, req.body, {
+                    runValidators: true,
+                })
 
-				if (updateRes.matchedCount < 1) {
-					res.status(404).json({
-						status: "404 Not Found",
-						message: `Cannot update non-existent id ${req.body._id}`
-					})
+                if (updateRes.matchedCount < 1) {
+                    res.status(404).json({
+                        status: "404 Not Found",
+                        message: `Cannot update non-existent id ${req.body._id}`,
+                    })
 
-					return
-				}
+                    return
+                }
 
-				if (updateRes.modifiedCount != updateRes.matchedCount) {
-					res.status(200).json({
-						status: "200 OK",
-						message: "Request succeeded but did not modify anything"
-					})
+                if (updateRes.modifiedCount != updateRes.matchedCount) {
+                    res.status(200).json({
+                        status: "200 OK",
+                        message: "Request succeeded but did not modify anything",
+                    })
 
-					return
-				}
+                    return
+                }
 
-				res.status(200).json({
-					status: "200 OK",
-					message: `Successfully updated id ${req.body._id}`,
-					receivedData: req.body
-				})
+                res.status(200).json({
+                    status: "200 OK",
+                    message: `Successfully updated id ${req.body._id}`,
+                    receivedData: req.body,
+                })
 
-				return
-			} catch (err) {
-				res.status(400).json({
-					status: "400 Bad Request",
-					message: (err as MongooseError).message,
-					receivedData: req.body
-				})
+                return
+            } catch (err) {
+                res.status(400).json({
+                    status: "400 Bad Request",
+                    message: (err as MongooseError).message,
+                    receivedData: req.body,
+                })
 
-				return
-			}
+                return
+            }
 
-		case "DELETE":
-			await connect()
+        case "DELETE":
+            await connect()
 
-			try {
-				const deleteRes = await ModelledObject.deleteOne({ _id: req.body._id })
+            try {
+                const deleteRes = await ModelledObject.deleteOne({ _id: req.body._id })
 
-				if (deleteRes.deletedCount < 1) {
-					res.status(404).json({
-						status: "404 Not Found",
-						message: `Cannot delete non-existent id ${req.body._id}`
-					})
+                if (deleteRes.deletedCount < 1) {
+                    res.status(404).json({
+                        status: "404 Not Found",
+                        message: `Cannot delete non-existent id ${req.body._id}`,
+                    })
 
-					return
-				}
+                    return
+                }
 
-				res.status(200).json({
-					status: "200 OK",
-					message: `Successfully deleted id ${req.body._id}`
-				})
+                res.status(200).json({
+                    status: "200 OK",
+                    message: `Successfully deleted id ${req.body._id}`,
+                })
 
-				return
-			} catch (err) {
-				res.status(400).json({
-					status: "400 Bad Request",
-					message: (err as MongooseError).message,
-					receivedData: req.body
-				})
+                return
+            } catch (err) {
+                res.status(400).json({
+                    status: "400 Bad Request",
+                    message: (err as MongooseError).message,
+                    receivedData: req.body,
+                })
 
-				return
-			}
+                return
+            }
 
-		default:
-			res.status(405).json({
-				status: "405 Method Not Allowed",
-				message: `Acceptable request methods include POST, PUT, and DELETE respectively, received ${method}`
-			})
+        default:
+            res.status(405).json({
+                status: "405 Method Not Allowed",
+                message: `Acceptable request methods include POST, PUT, and DELETE respectively, received ${method}`,
+            })
 
-			return
-	}
+            return
+    }
 }
--- src/templates/template-model.ts
+++ src/templates/template-model.ts
@@ -1,17 +1,17 @@
 import mongoose, { Schema } from "mongoose"
 
 const rawSchema = {
-	_id: {
-		type: String,
-		required: true,
-		validate: {
-			validator: function(input: string) {
-				return true
-			},
+    _id: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function (input: string) {
+                return true
+            },
 
-			message: "Template IDs must have the format format"
-		}
-	},
+            message: "Template IDs must have the format format",
+        },
+    },
 }
 
 const templateSchema = new Schema(rawSchema)
