7488b5660798e64c3368453a54329abe910039ab
500-internal-server-error <76838083+500-internal-server-error@users.noreply.github.com>
2022-09-25 14:05:02 +0700

    Templated API handlers

---
+++ src/pages/api/event2.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from "next"
+
+import { TemplateApiHandler } from "../../templates/template-api-path"
+
+import Event from "../../model/event"
+
+class EventApiHandler extends TemplateApiHandler {
+    public static override handle(req: NextApiRequest, res: NextApiResponse) {
+        super.handle(req, res)
+    }
+
+    protected static override getExpectedContentType() {
+        return "application/json"
+    }
+
+    protected static override getAcceptableMethods() {
+        return [
+            { method: "POST", handler: this.defaultHandlePost },
+            { method: "PUT", handler: this.defaultHandlePut },
+            { method: "DELETE", handler: this.defaultHandleDelete },
+        ]
+    }
+
+    protected static override getModelledObject() {
+        return Event
+    }
+}
+
+export default EventApiHandler.handle
