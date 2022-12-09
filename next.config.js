f9e4c8c21ea61177d927429c777eccbf77440c6f
mrmissx <hi@mrmiss.my.id>
2022-12-09 14:02:39 +0700

    feat: add redirect support
    Redirects file are taken from old repository (https://raw.githubusercontent.com/binusgdc/web/main/_redirects)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,30 @@
 /** @type {import('next').NextConfig} */
+
+const fs = require("fs")
+const redirectFile = "./_redirects"
+
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    redirects: async () => {
+        const redirects = await fs.promises.readFile(redirectFile, "utf-8")
+        const lines = redirects.split("\n")
+        return lines
+            .map((line) => {
+                if (!line.startsWith("/")) return
+
+                const data = line.trim().split(/\s+/) // handle unknown length of whitespace
+                if (data.length !== 2) {
+                    console.log("Failed to parse redirect: " + line)
+                    console.log(`Received: [${data.join(", ")}]`)
+                    console.log("Expected: [/source, destination]")
+                    return
+                }
+
+                return { source: data[0], destination: data[1], permanent: true }
+            })
+            .filter((e) => e !== undefined)
+    },
 }
 
 module.exports = nextConfig
