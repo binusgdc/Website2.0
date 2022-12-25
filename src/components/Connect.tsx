3d55e967e3d26c0810d9d01b1f86f05bf6503536
Aldi Atya Andika <aldiatya@gmail.com>
2022-12-25 15:20:57 +0700

    test

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -5,8 +5,8 @@ const Connect = () => {
                 <h1 className="mb-10 m-auto font-bold text-neutral-700 p-10 w-max lg:text-4xl text-xl">
                     Connect with us
                 </h1>
-                <div className="flex content-center justify-center">
-                    <form action="#" className="space-y-4 lg:w-2/5 w-2/3">
+                <div className="flex content-center justify-center ">
+                    <form action="#" className="space-y-4 lg:w-2/5 w-2/3 ">
                         <div className="lg:w-3/5 w-3/5 min-w-max">
                             <label
                                 htmlFor="subject"
@@ -17,50 +17,50 @@ const Connect = () => {
                             <input
                                 type="text"
                                 id="subject"
-                                className="block p-2 w-full  text-neutral-700 bg-gray-50 rounded-3xl "
+                                className="block p-2 w-full  text-neutral-700 bg-gray-100 rounded-lg "
                             />
                         </div>
 
                         <div className="lg:w-3/5 w-3/5 min-w-max">
                             <label
                                 htmlFor="email"
-                                className="block mb-2 font-bold text-neutral-700 "
+                                className="block mb-2 font-bold text-neutral-700 mt-8 "
                             >
                                 Email
                             </label>
                             <input
                                 type="email"
                                 id="email"
-                                className=" bg-gray-50  text-neutral-700  rounded-3xl block w-full p-2 "
+                                className=" bg-gray-100  text-neutral-700  rounded-lg block w-full p-2 "
                             />
                         </div>
                         <div className="lg:w-3/5 w-3/5 min-w-max">
                             <label
                                 htmlFor="subject"
-                                className="block mb-2  font-bold text-neutral-700 "
+                                className="block mb-2  font-bold text-neutral-700 mt-8 "
                             >
                                 Subject
                             </label>
                             <input
                                 type="text"
                                 id="subject"
-                                className="block p-2 w-full  text-neutral-700 bg-gray-50 rounded-3xl "
+                                className="bg-gray-100  text-neutral-700  rounded-lg block w-full p-2  "
                             />
                         </div>
                         <div className="sm:col-span-2">
                             <label
                                 htmlFor="message"
-                                className="block mb-2 font-bold text-neutral-700 "
+                                className="block mb-2 font-bold text-neutral-700 mt-8 "
                             >
                                 Description
                             </label>
                             <textarea
                                 id="message"
-                                className="block p-2.5 w-full  text-neutral-700 bg-gray-50 rounded-3xl   "
+                                className="bg-gray-100  text-neutral-700  rounded-lg block w-full p-2 bg-gray-100  text-neutral-700  rounded-lg block w-full p-2  "
                             ></textarea>
                         </div>
                         <div className=" flex justify-center ">
-                            <button className="flex justify-center bg-gradient-to-b from-amber-500 via-yellow-400 to-yellow-300 text-white text-sm font-medium py-1.5 px-4 rounded-3xl mt-10">
+                            <button className="flex justify-center bg-gradient-to-b from-amber-500 via-yellow-400 to-yellow-300 text-white text-sm font-medium py-1.5 px-4 rounded-2xl mt-10 mt-8 ">
                                 Submit
                             </button>
                         </div>
--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -2,15 +2,14 @@ const ContactUs = () => {
     return (
         <>
             <main className=" flex  content-center bg-neutral-700">
-                <div className=" pt-28 pb-32">
-                    <h1 className="m-auto font-bold text-white p-10 w-max lg:text-5xl text-xl">
+                <div className=" pt-28 pb-32 ">
+                    <h1 className="m-auto font-bold text-white p-10 w-max lg:text-5xl text-3xl ">
                         Get in Touch
                     </h1>
-                    <div className="pt-10 flex justify-center lg:flex-row flex-col  items-center ">
+                    <div className="pt-10 flex justify-center lg:flex-row flex-col  items-center lg:items-start align-top">
                         <div className="mr-8 ml-8 lg:w-1/6 w-2/5 flex justify-center flex-col items-center text-center">
-                            {" "}
-                            <img src="/discord.png" className="h-14 max-w-min" alt="" />{" "}
-                            <h1 className="mt-1 text-white font-bold">Community </h1>{" "}
+                            <img src="/discord.png" className="h-14 max-w-min" alt="" />
+                            <h1 className="mt-1 text-white font-bold">Community </h1>
                             <p className="mt-2 text-white text-xs font-light">
                                 Tempat dimana para game developer bertemu dan berbincang tentang
                                 hobi masing-masing. Terdapat banyak sub komunitas juga yang
@@ -18,9 +17,8 @@ const ContactUs = () => {
                             </p>
                         </div>
                         <div className="lg:mt-0 mt-20 mr-8 ml-8 lg:w-1/6 w-2/5 flex justify-center flex-col items-center text-center">
-                            {" "}
-                            <img src="/line.png" className="h-14 max-w-min" alt="" />{" "}
-                            <h1 className="mt-1 text-white font-bold">Line </h1>{" "}
+                            <img src="/line.png" className="h-14 max-w-min" alt="" />
+                            <h1 className="mt-1 text-white font-bold">Line </h1>
                             <p className="mt-2 text-white text-xs font-light">
                                 Tempat dimana para game developer bertemu dan berbincang tentang
                                 hobi masing-masing. Terdapat banyak sub komunitas juga yang
@@ -28,9 +26,8 @@ const ContactUs = () => {
                             </p>
                         </div>
                         <div className="lg:mt-0 mt-20 mr-8 ml-8 lg:w-1/6 w-2/5 flex justify-top flex-col items-center text-center">
-                            {" "}
-                            <img src="/email.png" className="h-14 max-w-min" alt="" />{" "}
-                            <h1 className="mt-1 text-white font-bold">Email </h1>{" "}
+                            <img src="/email.png" className="h-14 max-w-min" alt="" />
+                            <h1 className="mt-1 text-white font-bold">Email </h1>
                             <p className="mt-2 text-white text-xs text-center font-light">
                                 Bagi yang ingin berkolaborasi dan urusan penting lainnya bisa
                                 langsung menghubungi email kami di bgdc@binus.ac.id
