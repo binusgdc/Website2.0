9f00f3cbb39f5a85d1f3b2c23e2ad32c4fdc14d8
San <istamarahsan@gmail.com>
2023-04-20 07:01:52 +0700

    pengurus card component

--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -1,5 +1,94 @@
+import PengurusCard, { PengurusCardProps } from "../components/PengurusCard"
 import MainLayout from "../layouts/MainLayouts"
 import Image from "next/image"
+
+const pengurusStatic: PengurusCardProps[] = [
+    {
+        fullName: "Audrey Levina",
+        jabatanName: "Ketua Umum BGDC",
+        portraitImagePath: "/images/pengurus_img/Audrey Levina 2.jpg",
+    },
+    {
+        fullName: "Nathanael Matthew Makmur",
+        jabatanName: "Ketua Regional Kemanggisan",
+        portraitImagePath: "/images/pengurus_img/Nathanael Matthew Makmur 2.jpg",
+    },
+    {
+        fullName: "Vetri Marvel Budiman",
+        jabatanName: "Ketua Regional Alam Sutera",
+        portraitImagePath: "/images/pengurus_img/Vetri Marvel Budiman 2 edited.png",
+    },
+    {
+        fullName: "Robertus Eric",
+        jabatanName: "Sekretaris Umum",
+        portraitImagePath: "/images/pengurus_img/Robertus Eric 2.jpg",
+    },
+    {
+        fullName: "Dheanandri",
+        jabatanName: "Bendahara Umum",
+        portraitImagePath: "/images/pengurus_img/Dheanandri.jpg",
+    },
+    {
+        fullName: "Justin Christian",
+        jabatanName: "Database Manager",
+        portraitImagePath: "/images/pengurus_img/Justin Christian.jpg",
+    },
+    {
+        fullName: "Daniel Alexander",
+        jabatanName: "Koordinator Community Development Division",
+        portraitImagePath: "/images/pengurus_img/Daniel Alexander 2.jpg",
+    },
+    {
+        fullName: "Rionaldi Marhanson Wijaya",
+        jabatanName: "Sub-Koordinator Community Development Division",
+        portraitImagePath: "/images/pengurus_img/Rionaldi Marhanson Wijaya 3.jpg",
+    },
+    {
+        fullName: "Ariel Vito Suyata Ong",
+        jabatanName: "Sub-Koordinator Community Development Division",
+        portraitImagePath: "/images/pengurus_img/Ariel Vito Suyata Ong.jpg",
+    },
+    {
+        fullName: "Richard Prayogi",
+        jabatanName: "Koordinator Education Division",
+        portraitImagePath: "/images/pengurus_img/Richard Prayogi 2.jpg",
+    },
+    {
+        fullName: "Gaung Ramadhan Bagaswara",
+        jabatanName: "Sub-Koordinator Education Division",
+        portraitImagePath: "/images/pengurus_img/Gaung Ramadhan Bagaswara 2.jpg",
+    },
+    {
+        fullName: "Winson Allan Wijaya",
+        jabatanName: "Sub-Koordinator Education Division",
+        portraitImagePath: "/images/pengurus_img/Winson Allan Wijaya.jpg",
+    },
+    {
+        fullName: "Hubertus Darrel Santoso",
+        jabatanName: "Koordinator Public Relations Division",
+        portraitImagePath: "/images/pengurus_img/Hubertus Darrel Santoso 2.jpeg",
+    },
+    {
+        fullName: "Vito Sebastian Sanjaya",
+        jabatanName: "Sub-Koordinator Public Relations Division",
+        portraitImagePath: "/images/pengurus_img/Vito Sebastian Sanjaya 2.jpg",
+    },
+    {
+        fullName: "Ferry",
+        jabatanName: "Sub-Koordinator Public Relations Division",
+        portraitImagePath: "/images/pengurus_img/Ferry Edited.png",
+    },
+    {
+        fullName: "Stanly Bianconeri",
+        jabatanName: "Koordinator Media and Creative Division",
+        portraitImagePath: "/images/pengurus_img/Stanly Bianconeri 2.jpg",
+    },
+    {
+        fullName: "Steven Yoshell Yapriadi",
+        jabatanName: "HR Manager",
+        portraitImagePath: "/images/pengurus_img/Steven Yoshell Yapriadi 2.png",
+    },
+]
 
 function About() {
     return (
@@ -166,286 +255,9 @@ function About() {
                     <h3 className="text-sm md:text-base font-bold">Orang-Orang Dibalik BGDC</h3>
                 </div>
                 <div className="px-8 lg:px-auto m-4 grid grid-cols-1 gap-8 sm:grid-cols-3 ">
-                    <div className="text-center pt-8 ">
-                        <div className="max-w-xs max-h-xs bg-white grid place-items-center">
-                            <Image
-                                className="rounded-xl object-cover"
-                                src="/images/pengurus_img/Audrey Levina 2.jpg"
-                                alt="blog 1"
-                                width={175}
-                                height={225}
-                            />
-                        </div>
-                        <h1 className="my-4 font-bold break-normal text-lg md:text-lg">
-                            Audrey Levina
-                        </h1>
-                        <p className="text-sm md:text-sm font-semibold">Ketua Umum BGDC</p>
-                    </div>
-
-                    <div className="text-center pt-8 ">
-                        <div className="max-w-xs max-h-xs bg-white grid place-items-center">
-                            <Image
-                                className="rounded-xl object-cover"
-                                src="/images/pengurus_img/Nathanael Matthew Makmur 2.jpg"
-                                alt="blog 1"
-                                width={175}
-                                height={225}
-                            />
-                        </div>
-                        <h1 className="my-4 font-bold break-normal text-lg md:text-lg">
-                            Nathanael Matthew Makmur
-                        </h1>
-                        <p className="text-sm md:text-sm font-semibold">
-                            Ketua Regional Kemanggisan
-                        </p>
-                    </div>
-
-                    <div className="text-center pt-8 ">
-                        <div className="max-w-xs max-h-xs bg-white grid place-items-center">
-                            <Image
-                                className="rounded-xl object-cover"
-                                src="/images/pengurus_img/Vetri Marvel Budiman 2 edited.png"
-                                alt="blog 1"
-                                width={175}
-                                height={225}
-                            />
-                        </div>
-                        <h1 className="my-4 font-bold break-normal text-lg md:text-lg">
-                            Vetri Marvel Budiman
-                        </h1>
-                        <p className="text-sm md:text-sm font-semibold">
-                            Ketua Regional Alam Sutera
-                        </p>
-                    </div>
-                    <div className="text-center pt-8 ">
-                        <div className="max-w-xs max-h-xs bg-white grid place-items-center">
-                            <Image
-                                className="rounded-xl object-cover"
-                                src="/images/pengurus_img/Robertus Eric 2.jpg"
-                                alt="blog 1"
-                                width={175}
-                                height={225}
-                            />
-                        </div>
-                        <h1 className="my-4 font-bold break-normal text-lg md:text-lg">
-                            Robertus Eric
-                        </h1>
-                        <p className="text-sm md:text-sm font-semibold">Sekretaris Umum</p>
-                    </div>
-
-                    <div className="text-center pt-8 ">
-                        <div className="max-w-xs max-h-xs bg-white grid place-items-center">
-                            <Image
-                                className="rounded-xl object-cover"
-                                src="/images/pengurus_img/Dheanandri.jpg"
-                                alt="blog 1"
-                                width={175}
-                                height={225}
-                            />
-                        </div>
-                        <h1 className="my-4 font-bold break-normal text-lg md:text-lg">
-                            Dheanandri
-                        </h1>
-                        <p className="text-sm md:text-sm font-semibold">Bendahara Umum</p>
-                    </div>
-                    <div className="text-center pt-8 ">
-                        <div className="max-w-xs max-h-xs bg-white grid place-items-center">
-                            <Image
-                                className="rounded-xl object-cover"
-                                src="/images/pengurus_img/Justin Christian.jpg"
-                                alt="blog 1"
-                                width={175}
-                                height={225}
-                            />
-                        </div>
-                        <h1 className="my-4 font-bold break-normal text-lg md:text-lg">
-                            Justin Christian
-                        </h1>
-                        <p className="text-sm md:text-sm font-semibold">Database Manager</p>
-                    </div>
-                    <div className="text-center pt-8 ">
-                        <div className="max-w-xs max-h-xs bg-white grid place-items-center">
-                            <Image
-                                className="rounded-xl object-cover"
-                                src="/images/pengurus_img/Daniel Alexander 2.jpg"
-                                alt="blog 1"
-                                width={175}
-                                height={225}
-                            />
-                        </div>
-                        <h1 className="my-4 font-bold break-normal text-lg md:text-lg">
-                            Daniel Alexander
-                        </h1>
-                        <p className="text-sm md:text-sm font-semibold">
-                            Koordinator Community Development Division
-                        </p>
-                    </div>
-                    <div className="text-center pt-8 ">
-                        <div className="max-w-xs max-h-xs bg-white grid place-items-center">
-                            <Image
-                                className="rounded-xl object-cover"
-                                src="/images/pengurus_img/Rionaldi Marhanson Wijaya 3.jpg"
-                                alt="blog 1"
-                                width={175}
-                                height={225}
-                            />
-                        </div>
-                        <h1 className="my-4 font-bold break-normal text-lg md:text-lg">
-                            Rionaldi Marhanson Wijaya
-                        </h1>
-                        <p className="text-sm md:text-sm font-semibold">
-                            Sub-Koordinator Community Development Division
-                        </p>
-                    </div>
-                    <div className="text-center pt-8 ">
-                        <div className="max-w-xs max-h-xs bg-white grid place-items-center">
-                            <Image
-                                className="rounded-xl object-cover"
-                                src="/images/pengurus_img/Ariel Vito Suyata Ong.jpg"
-                                alt="blog 1"
-                                width={175}
-                                height={225}
-                            />
-                        </div>
-                        <h1 className="my-4 font-bold break-normal text-lg md:text-lg">
-                            Ariel Vito Suyata Ong
-                        </h1>
-                        <p className="text-sm md:text-sm font-semibold">
-                            Sub-Koordinator Community Development Division
-                        </p>
-                    </div>
-                    <div className="text-center pt-8 ">
-                        <div className="max-w-xs max-h-xs bg-white grid place-items-center">
-                            <Image
-                                className="rounded-xl object-cover"
-                                src="/images/pengurus_img/Richard Prayogi 2.jpg"
-                                alt="blog 1"
-                                width={175}
-                                height={225}
-                            />
-                        </div>
-                        <h1 className="my-4 font-bold break-normal text-lg md:text-lg">
-                            Richard Prayogi
-                        </h1>
-                        <p className="text-sm md:text-sm font-semibold">
-                            Koordinator Education Division
-                        </p>
-                    </div>
-                    <div className="text-center pt-8 ">
-                        <div className="max-w-xs max-h-xs bg-white grid place-items-center">
-                            <Image
-                                className="rounded-xl object-cover"
-                                src="/images/pengurus_img/Gaung Ramadhan Bagaswara 2.jpg"
-                                alt="blog 1"
-                                width={175}
-                                height={225}
-                            />
-                        </div>
-                        <h1 className="my-4 font-bold break-normal text-lg md:text-lg">
-                            Gaung Ramadhan Bagaswara
-                        </h1>
-                        <p className="text-sm md:text-sm font-semibold">
-                            Sub-Koordinator Education Division
-                        </p>
-                    </div>
-                    <div className="text-center pt-8 ">
-                        <div className="max-w-xs max-h-xs bg-white grid place-items-center">
-                            <Image
-                                className="rounded-xl object-cover"
-                                src="/images/pengurus_img/Winson Allan Wijaya.jpg"
-                                alt="blog 1"
-                                width={175}
-                                height={225}
-                            />
-                        </div>
-                        <h1 className="my-4 font-bold break-normal text-lg md:text-lg">
-                            Winson Allan Wijaya
-                        </h1>
-                        <p className="text-sm md:text-sm font-semibold">
-                            Sub-Koordinator Education Division
-                        </p>
-                    </div>
-                    <div className="text-center pt-8 ">
-                        <div className="max-w-xs max-h-xs bg-white grid place-items-center">
-                            <Image
-                                className="rounded-xl object-cover"
-                                src="/images/pengurus_img/Hubertus Darrel Santoso 2.jpeg"
-                                alt="blog 1"
-                                width={175}
-                                height={225}
-                            />
-                        </div>
-                        <h1 className="my-4 font-bold break-normal text-lg md:text-lg">
-                            Hubertus Darrel Santoso
-                        </h1>
-                        <p className="text-sm md:text-sm font-semibold">
-                            Koordinator Public Relations Division
-                        </p>
-                    </div>
-                    <div className="text-center pt-8 ">
-                        <div className="max-w-xs max-h-xs bg-white grid place-items-center">
-                            <Image
-                                className="rounded-xl object-cover"
-                                src="/images/pengurus_img/Vito Sebastian Sanjaya 2.jpg"
-                                alt="blog 1"
-                                width={175}
-                                height={225}
-                            />
-                        </div>
-                        <h1 className="my-4 font-bold break-normal text-lg md:text-lg">
-                            Vito Sebastian Sanjaya
-                        </h1>
-                        <p className="text-sm md:text-sm font-semibold">
-                            Sub-Koordinator Public Relations Division
-                        </p>
-                    </div>
-                    <div className="text-center pt-8 ">
-                        <div className="max-w-xs max-h-xs bg-white grid place-items-center">
-                            <Image
-                                className="rounded-xl object-cover"
-                                src="/images/pengurus_img/Ferry Edited.png"
-                                alt="blog 1"
-                                width={175}
-                                height={225}
-                            />
-                        </div>
-                        <h1 className="my-4 font-bold break-normal text-lg md:text-lg">Ferry</h1>
-                        <p className="text-sm md:text-sm font-semibold">
-                            Sub-Koordinator Public Relations Division
-                        </p>
-                    </div>
-                    <div className="text-center pt-8 ">
-                        <div className="max-w-xs max-h-xs bg-white grid place-items-center">
-                            <Image
-                                className="rounded-xl object-cover"
-                                src="/images/pengurus_img/Stanly Bianconeri 2.jpg"
-                                alt="blog 1"
-                                width={175}
-                                height={225}
-                            />
-                        </div>
-                        <h1 className="my-4 font-bold break-normal text-lg md:text-lg">
-                            Stanly Bianconeri
-                        </h1>
-                        <p className="text-sm md:text-sm font-semibold">
-                            Koordinator Media and Creative Division
-                        </p>
-                    </div>
-                    <div className="text-center pt-8 ">
-                        <div className="max-w-xs max-h-xs bg-white grid place-items-center">
-                            <Image
-                                className="rounded-xl object-cover"
-                                src="/images/pengurus_img/Steven Yoshell Yapriadi 2.png"
-                                alt="blog 1"
-                                width={175}
-                                height={225}
-                            />
-                        </div>
-                        <h1 className="my-4 font-bold break-normal text-lg md:text-lg">
-                            Steven Yoshell Yapriadi
-                        </h1>
-                        <p className="text-sm md:text-sm font-semibold">HR Manager</p>
-                    </div>
+                    {pengurusStatic.map((props, i) => (
+                        <PengurusCard key={i} {...props}></PengurusCard>
+                    ))}
                 </div>
             </div>
         </MainLayout>
