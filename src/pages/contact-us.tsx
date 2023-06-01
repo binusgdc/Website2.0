eaa928be027db26de8bdc3c4476efc86af712d08
San <istamarahsan@gmail.com>
2023-06-01 19:35:18 +0700

    remove nonfunctional connect with us

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -92,8 +92,28 @@ function Contact() {
                         <div className="flex mt-14 ml-10 flex-col lg:mb-0 mb-20">
                             <h1 className="text-lg font-bold text-neutral-700 ">Other Socials</h1>
 
-                            <div className="flex mt-3  lg:space-x-4 space-x-2">
-                                <a href="#" className="text-gray-900 ">
+                            <div className="flex mt-3 gap-1 lg:space-x-4 space-x-2">
+                                <a
+                                    href="https://instagram.com/binusgdc"
+                                    className="text-gray-900 "
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                >
+                                    <svg
+                                        className="w-5 h-5 "
+                                        fill="currentColor"
+                                        viewBox="0 0 24 24"
+                                        aria-hidden="true"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                    <span className="sr-only">Instagram page</span>
+                                </a>
+                                <a href="https://bgdc.itch.io/" className="text-gray-900 ">
                                     <svg
                                         className="w-5 h-5 "
                                         fill="currentColor"
@@ -109,7 +129,10 @@ function Contact() {
                                     <span className="sr-only">Itch</span>
                                 </a>
 
-                                <a href="#" className="text-gray-900 ">
+                                <a
+                                    href="https://www.linkedin.com/company/binus-game-development-club/"
+                                    className="text-gray-900 "
+                                >
                                     <svg
                                         className="w-5 h-5"
                                         fill="currentColor"
@@ -125,7 +148,10 @@ function Contact() {
                                     <span className="sr-only">LinkedIn Page</span>
                                 </a>
 
-                                <a href="#" className="text-gray-900 ">
+                                <a
+                                    href="https://www.youtube.com/channel/UCdDgF6FOjsIfKinPaorLF4Q"
+                                    className="text-gray-900 "
+                                >
                                     <svg
                                         className="w-5 h-5 "
                                         fill="currentColor"
@@ -141,7 +167,7 @@ function Contact() {
                                     <span className="sr-only">YouTube Page</span>
                                 </a>
 
-                                <a href="#" className="text-gray-900">
+                                <a href="https://twitter.com/Binusgdc" className="text-gray-900">
                                     <svg
                                         className="w-5 h-5 "
                                         fill="currentColor"
@@ -153,7 +179,10 @@ function Contact() {
                                     <span className="sr-only">Twitter page</span>
                                 </a>
 
-                                <a href="#" className="text-gray-900">
+                                <a
+                                    href="https://www.facebook.com/BinusGDC/"
+                                    className="text-gray-900"
+                                >
                                     <svg
                                         className="w-5 h-5 "
                                         fill="currentColor"
@@ -182,72 +211,6 @@ function Contact() {
                             ></iframe>
                         </a>
                     </div>
-                </div>
-            </div>
-            <div className=" pt-4 pb-10">
-                <h1 className="mb-10 m-auto font-bold text-neutral-700 p-10 w-max lg:text-4xl text-xl">
-                    Connect with us
-                </h1>
-                <div className="flex content-center justify-center ">
-                    <form action="#" className="space-y-4 lg:w-2/5 w-2/3 ">
-                        <div className="lg:w-3/5 w-3/5 min-w-max">
-                            <label
-                                htmlFor="subject"
-                                className="block mb-2 font-bold text-neutral-700 "
-                            >
-                                Name
-                            </label>
-                            <input
-                                type="text"
-                                id="subject"
-                                className="block p-2 w-full  text-neutral-700 bg-gray-100 rounded-lg "
-                            />
-                        </div>
-
-                        <div className="lg:w-3/5 w-3/5 min-w-max">
-                            <label
-                                htmlFor="email"
-                                className="block mb-2 font-bold text-neutral-700 mt-8 "
-                            >
-                                Email
-                            </label>
-                            <input
-                                type="email"
-                                id="email"
-                                className=" bg-gray-100  text-neutral-700  rounded-lg block w-full p-2 "
-                            />
-                        </div>
-                        <div className="lg:w-3/5 w-3/5 min-w-max">
-                            <label
-                                htmlFor="subject"
-                                className="block mb-2  font-bold text-neutral-700 mt-8 "
-                            >
-                                Subject
-                            </label>
-                            <input
-                                type="text"
-                                id="subject"
-                                className="bg-gray-100  text-neutral-700  rounded-lg block w-full p-2  "
-                            />
-                        </div>
-                        <div className="sm:col-span-2">
-                            <label
-                                htmlFor="message"
-                                className="block mb-2 font-bold text-neutral-700 mt-8 "
-                            >
-                                Description
-                            </label>
-                            <textarea
-                                id="message"
-                                className="bg-gray-100  text-neutral-700  rounded-lg block w-full p-2 bg-gray-100  text-neutral-700  rounded-lg block w-full p-2  "
-                            ></textarea>
-                        </div>
-                        <div className=" flex justify-center ">
-                            <button className="flex justify-center bg-gradient-to-b from-amber-500 via-yellow-400 to-yellow-300 text-white text-sm font-medium py-1.5 px-4 rounded-2xl mt-10 mt-8 ">
-                                Submit
-                            </button>
-                        </div>
-                    </form>
                 </div>
             </div>
         </MainLayout>
