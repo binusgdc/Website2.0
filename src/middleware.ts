37bc2391e7b62f501905b17928d017f9ed039462
San <istamarahsan@gmail.com>
2023-06-03 22:24:41 +0700

    pathname not basepath

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server"
 
 export default function middleware(req: NextRequest) {
-    if (req.nextUrl.basePath === "/balls") {
+    if (req.nextUrl.pathname === "/balls") {
         return NextResponse.redirect(new URL("https://www.google.com/"))
     }
 }
