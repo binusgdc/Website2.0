32bcd0741311a2924ff93a52f5217f1cd5925087
San <istamarahsan@gmail.com>
2023-06-05 13:49:23 +0700

    use build-time prefix matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,18 +2,19 @@ import { NextRequest, NextResponse } from "next/server"
 import { env } from "./env.mjs"
 
 export default async function middleware(req: NextRequest) {
-    const prefix = "/go"
-    if (req.nextUrl.pathname.startsWith(prefix)) {
-        const redirect = await fetch(
-            `${env.CANONICAL_BASE_URL}/api/redirects/${encodeURIComponent(
-                req.nextUrl.pathname.slice(prefix.length)
-            )}`
-        )
-        if (redirect.status === 200) {
-            const { targetUrl } = await redirect.json()
-            if (typeof targetUrl === "string") {
-                return NextResponse.redirect(targetUrl)
-            }
+    const redirect = await fetch(
+        `${env.CANONICAL_BASE_URL}/api/redirects/${encodeURIComponent(
+            req.nextUrl.pathname.slice("/link".length)
+        )}`
+    )
+    if (redirect.status === 200) {
+        const { targetUrl } = await redirect.json()
+        if (typeof targetUrl === "string") {
+            return NextResponse.redirect(targetUrl)
         }
     }
+}
+
+export const config = {
+    matcher: "/link/:path*",
 }
