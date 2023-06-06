5792832c8363c33f7d5406c45ed46a6007a6021a
San <istamarahsan@gmail.com>
2023-06-06 16:55:25 +0700

    canonical base url part of redirect env

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,15 +2,17 @@ import { NextRequest, NextResponse } from "next/server"
 import { env } from "./env.mjs"
 
 export default async function middleware(req: NextRequest) {
-    const redirect = await fetch(
-        `${env.CANONICAL_BASE_URL}/api/redirects/${encodeURIComponent(
-            req.nextUrl.pathname.slice("/link".length)
-        )}`
-    )
-    if (redirect.status === 200) {
-        const { targetUrl } = await redirect.json()
-        if (typeof targetUrl === "string") {
-            return NextResponse.redirect(targetUrl)
+    if (env.CANONICAL_BASE_URL !== undefined) {
+        const redirect = await fetch(
+            `${env.CANONICAL_BASE_URL}/api/redirects/${encodeURIComponent(
+                req.nextUrl.pathname.slice("/link".length)
+            )}`
+        )
+        if (redirect.status === 200) {
+            const { targetUrl } = await redirect.json()
+            if (typeof targetUrl === "string") {
+                return NextResponse.redirect(targetUrl)
+            }
         }
     }
 }
