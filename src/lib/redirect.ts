80e6ff0a7e116e72c2d5cca0889597bd8ea3d26c
San <istamarahsan@gmail.com>
2023-06-11 20:37:28 +0700

    redirect data: multiple matches returns first, no add/remove

--- src/lib/redirect.ts
+++ src/lib/redirect.ts
@@ -7,11 +7,9 @@ export type Redirect = {
     targetUrl: URL
 }
 
-export interface RedirectsData {
-    createIfNotExists(redirect: Redirect): Promise<boolean>
+interface RedirectsData {
     getAll(): Promise<Redirect[]>
     getForPath(sourcePath: string): Promise<Redirect | undefined>
-    removeIfExists(sourcePath: string): Promise<void>
 }
 
 export class RedirectsService {
@@ -34,22 +32,11 @@ export class RedirectsInMemory implements RedirectsData {
     constructor() {
         this.cache = new Map()
     }
-    createIfNotExists(redirect: Redirect): Promise<boolean> {
-        if (this.cache.has(redirect.sourcePath)) {
-            return Promise.resolve(false)
-        }
-        this.cache.set(redirect.sourcePath, redirect)
-        return Promise.resolve(true)
-    }
     getAll(): Promise<Redirect[]> {
         return Promise.resolve(Array.from(this.cache.values()))
     }
     getForPath(sourcePath: string): Promise<Redirect | undefined> {
         return Promise.resolve(this.cache.get(sourcePath))
-    }
-    removeIfExists(sourcePath: string): Promise<void> {
-        this.cache.delete(sourcePath)
-        return Promise.resolve()
     }
 }
 
@@ -59,33 +46,6 @@ export class RedirectsAirtable implements RedirectsData {
     constructor(base: AirtableBase, redirectsTableId: string) {
         this.base = base
         this.redirectsTableId = redirectsTableId
-    }
-    async removeIfExists(sourcePath: string): Promise<void> {
-        const recordToRemoveResult = await this.base(this.redirectsTableId)
-            .select({
-                filterByFormula: `{Source Path} = '${sourcePath}'`,
-                maxRecords: 1,
-            })
-            .all()
-        if (recordToRemoveResult.length !== 1) {
-            throw new Error("Expected exactly one record to match the source path")
-        }
-        await this.base(this.redirectsTableId).destroy(recordToRemoveResult[0].id)
-    }
-    async createIfNotExists(redirect: Redirect): Promise<boolean> {
-        const existingRedirect = await this.getForPath(redirect.sourcePath)
-        if (existingRedirect) {
-            return false
-        }
-        await this.base(this.redirectsTableId).create([
-            {
-                fields: {
-                    "Source Path": redirect.sourcePath,
-                    "Target URL": redirect.targetUrl.toString(),
-                },
-            },
-        ])
-        return true
     }
     async getAll(): Promise<Redirect[]> {
         const records = await this.base(this.redirectsTableId).select().all()
@@ -100,23 +60,24 @@ export class RedirectsAirtable implements RedirectsData {
         if (recordResult.length === 0) {
             return undefined
         }
-        if (recordResult.length > 1) {
-            throw new Error("Expected at most one record to match the source path")
-        }
         return this.parseRedirectRecord(recordResult[0])
     }
 
     private parseRedirectRecord(record: any): Redirect {
-        const sourcePath = record.get("Source Path")?.toString()
-        const targetUrl = record.get("Target Url")?.toString()
-        if (!sourcePath || !targetUrl) {
+        try {
+            const sourcePath = record.get("Source Path")?.toString()
+            const targetUrl = record.get("Target Url")?.toString()
+            if (!sourcePath || !targetUrl) {
+                throw {}
+            }
+            return {
+                sourcePath: sourcePath,
+                targetUrl: targetUrl,
+            }
+        } catch (_) {
             throw new Error(
-                "Expected | Source Path | and | Target URL | to be string-interpretable fields"
+                `Expected "Source Path" and "Target URL" to be string-interpretable fields on the base.`
             )
-        }
-        return {
-            sourcePath: sourcePath,
-            targetUrl: targetUrl,
         }
     }
 }
