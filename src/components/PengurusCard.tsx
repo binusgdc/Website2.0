823cf6163986b6cc75f0f36e3deacd4f023e6675
istamarahsan <istamarahsan@gmail.com>
2023-09-23 08:27:19 +0700

    simple fade in on image load

--- src/components/PengurusCard.tsx
+++ src/components/PengurusCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { useState } from "react"
 
 export type PengurusCardProps = {
     fullName: string
@@ -7,11 +8,13 @@ export type PengurusCardProps = {
 }
 
 const PengurusCard = ({ fullName, jabatanName, portraitImagePath }: PengurusCardProps) => {
+    const [isLoaded, setLoaded] = useState(false)
     return (
         <div className="text-center pt-4 grid place-items-center">
-            <div className="border-orange-300 bg-orange-300 w-[200px] h-[200px] rounded-full border-4">
+            <div className={`border-orange-300 bg-orange-300 w-[200px] h-[200px] rounded-full border-4`}>
                 <Image
-                    className="object-cover"
+                    onLoad={(_e) => {setLoaded(true)}}
+                    className={`object-cover transition-opacity ${isLoaded ? "opacity-100" : "opacity-0"}`} 
                     src={portraitImagePath}
                     alt={fullName}
                     width={200}
